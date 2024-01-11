66496bc555c179a85a2925e2f2d42c14d88bad83
Luiz Comparin <luizcomparin18@gmail.com>
2024-01-11 01:44:26 -0300

    change: add module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { CadastrarClienteComponent } from './pages/cadastrar-cliente/cadastrar-cliente.component';
+import { ListarClientesComponent } from './pages/listar-clientes/listar-clientes.component';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { InputComponent } from './components/input/input.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+import { TooltipModule } from 'primeng/tooltip';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        SidebarComponent,
+        CadastrarClienteComponent,
+        ListarClientesComponent,
+        PageHeaderComponent,
+        InputComponent
+    ],
+    imports: [
+        NgxMaskDirective,
+        NgxMaskPipe,
+        TooltipModule,
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
+    ],
+    providers: [
+        provideNgxMask(),
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
