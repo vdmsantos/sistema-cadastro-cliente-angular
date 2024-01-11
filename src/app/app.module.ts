47daf1432c8bf2dd75ebabd0d3612055434448bd
Luiz Comparin <luizcomparin18@gmail.com>
2024-01-11 14:04:08 -0300

    change: app module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
+// MODULES
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TooltipModule } from 'primeng/tooltip';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// COMPONENTS
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { CadastrarClienteComponent } from './pages/cadastrar-cliente/cadastrar-cliente.component';
 import { ListarClientesComponent } from './pages/listar-clientes/listar-clientes.component';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
 import { InputComponent } from './components/input/input.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ListComponent } from './components/list/list.component';
+// OTHERS
 import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
-import { TooltipModule } from 'primeng/tooltip';
+import { ConfirmationService, MessageService } from 'primeng/api';
 
 @NgModule({
     declarations: [
@@ -19,19 +26,25 @@ import { TooltipModule } from 'primeng/tooltip';
         CadastrarClienteComponent,
         ListarClientesComponent,
         PageHeaderComponent,
-        InputComponent
+        InputComponent,
+        ListComponent
     ],
     imports: [
         NgxMaskDirective,
         NgxMaskPipe,
         TooltipModule,
         BrowserModule,
+        BrowserAnimationsModule,
         AppRoutingModule,
         FormsModule,
         ReactiveFormsModule,
+        MatDialogModule,
+        ConfirmPopupModule,
     ],
     providers: [
         provideNgxMask(),
+        MessageService,
+        ConfirmationService,
     ],
     bootstrap: [AppComponent]
 })
