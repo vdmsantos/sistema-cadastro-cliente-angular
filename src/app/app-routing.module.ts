8c9e35edc14bbf03dd1f1c776cb87821cf6065b1
Luiz Comparin <luizcomparin18@gmail.com>
2024-01-11 01:45:48 -0300

    feat: app routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ListarClientesComponent } from './pages/listar-clientes/listar-clientes.component';
+import { CadastrarClienteComponent } from './pages/cadastrar-cliente/cadastrar-cliente.component';
 
-const routes: Routes = [];
+export const APP_ROUTE_PATHS = {
+    LISTAR_CLIENTES: 'listar-clientes',
+    CADASTRAR_CLIENTE: 'cadastrar-cliente',
+}
+
+const routes: Routes = [
+    { path: '', redirectTo: APP_ROUTE_PATHS.LISTAR_CLIENTES, pathMatch: 'full' },
+    { path: APP_ROUTE_PATHS.LISTAR_CLIENTES, component: ListarClientesComponent },
+    { path: APP_ROUTE_PATHS.CADASTRAR_CLIENTE, component: CadastrarClienteComponent },
+    { path: '**', redirectTo: APP_ROUTE_PATHS.LISTAR_CLIENTES, pathMatch: 'prefix' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
