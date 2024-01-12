ec42b77def69e7f232cf996fe922a22a20ad7a5e
Luiz Comparin <luizcomparin18@gmail.com>
2024-01-12 16:01:33 -0300

    feat: add 10 customers & clear customer list methods

--- src/app/pages/listar-clientes/listar-clientes.component.ts
+++ src/app/pages/listar-clientes/listar-clientes.component.ts
@@ -1,23 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ClienteEntity } from '../../entities/cliente.entity';
 import { ClienteService } from '../../services/cliente.service';
 import { PaginationOptionsType } from '../../types/paginationTypes/pagination-options.type';
 import { PaginationService } from '../../services/pagination.service';
+import { HelperService } from '../../services/helper.service';
+import { PrimeIcons } from 'primeng/api';
 
 @Component({
     selector: 'app-listar-clientes',
     templateUrl: './listar-clientes.component.html',
     styleUrls: ['./listar-clientes.component.scss']
 })
-export class ListarClientesComponent {
+export class ListarClientesComponent implements OnInit {
     constructor(
         private clienteService: ClienteService,
         private paginationService: PaginationService,
+        private helperService: HelperService,
     ) { }
+
+    ngOnInit(): void {
+        this.fetchClientesList()
+    }
 
     clientesListSignal = this.clienteService.getClientesListSignal
     paginationMetaSignal = this.paginationService.getPaginationMetaSignal
     lastPaginationOptionsUsedSig = this.paginationService.getLastPaginationOptionsUsedSig
+    PrimeIcons = PrimeIcons
 
     list_tableFields: { label: string, column: string }[] = [
         {
@@ -67,7 +75,7 @@ export class ListarClientesComponent {
     ]
 
     fetchClientesList(
-        { page, limit, order, orderByColumn, searchQuery, searchBy, startByOrContain }: PaginationOptionsType
+        { page, limit, order, orderByColumn, searchQuery, searchBy, startByOrContain }: PaginationOptionsType = {}
     ) {
         return this.clienteService.fetchClientesList({
             page: page,
@@ -92,5 +100,37 @@ export class ListarClientesComponent {
                 numero: item.endereco.numero,
             }
         })
+    }
+
+    getRandomName() {
+        console.log('randomName', this.helperService.getRandomName())
+    }
+
+    add10Clientes() {
+        const getNewCliente = () => new ClienteEntity({
+            atualizado_em: new Date().toISOString(),
+            criado_em: new Date().toISOString(),
+            cpf: 'asd',
+            id: this.helperService.newUUID(),
+            nome: this.helperService.getRandomName(),
+            telefone: '123456789',
+            endereco: {
+                bairro: 'aleatorio',
+                cep: '89012060',
+                cidade: 'aleatorio',
+                estado: 'aleatorio',
+                numero: '123',
+                rua: 'aleatorio'
+            },
+        })
+        const _10NewClientes = [getNewCliente(), getNewCliente(), getNewCliente(), getNewCliente(), getNewCliente(), getNewCliente(), getNewCliente(), getNewCliente(), getNewCliente(), getNewCliente(),]
+        _10NewClientes.forEach(cliente => {
+            this.clienteService.createOrUpdate(cliente)
+        })
+        this.fetchClientesList()
+    }
+
+    clearClienteList() {
+        this.clienteService.clearClienteList()
     }
 }
