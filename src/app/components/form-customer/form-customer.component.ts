1156e91c25e61b5a4a2749520fcd9c5dac5624c7
Luiz Comparin <luizcomparin18@gmail.com>
2024-01-13 17:18:09 -0300

    feat: add cep related methods

--- src/app/components/form-customer/form-customer.component.ts
+++ src/app/components/form-customer/form-customer.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { ClienteFormService } from '../../services/cliente-form.service';
-import { MessageService } from 'primeng/api';
+import { CepService } from '../../services/cep.service';
+import { take } from 'rxjs';
+import { PrimeIcons } from 'primeng/api';
+import { Endereco } from '../../types/endereco.interface';
 
 @Component({
     selector: 'app-form-customer',
@@ -11,13 +14,44 @@ export class FormCustomerComponent {
 
     constructor(
         private clienteFormService: ClienteFormService,
+        private cepService: CepService,
     ) { }
 
     clienteFormGroup = this.clienteFormService.getClienteFormGroup()
     getClienteFormControl = this.clienteFormService.getClienteFormControl.bind(this.clienteFormService);
     getClienteEnderecoFormControl = this.clienteFormService.getClienteEnderecoFormControl.bind(this.clienteFormService);
+    cepResponseSignal = this.cepService.cepResponseSignal
+    PrimeIcons = PrimeIcons
+    loadingCep = false
+    cepErrorMessage = ''
 
     submitForm() {
         this.clienteFormService.submit()
+    }
+
+    getCepAndSetClienteEnderecoForm(cep: string) {
+        this.loadingCep = true
+        this.cepService.getCepObservable(cep).pipe(
+            take(1)
+        ).subscribe({
+            next: (res) => {
+                const newEndereco = {
+                    bairro: res.neighborhood,
+                    cep: res.cep,
+                    cidade: res.city,
+                    estado: res.state,
+                    numero: this.getClienteEnderecoFormControl('numero').value,
+                    rua: res.street
+                } as Endereco
+                this.clienteFormService.setClienteEndereco(newEndereco)
+                this.cepErrorMessage = ''
+                this.loadingCep = false
+            },
+            error: (err) => {
+                console.log(`Erro ao consultar cep '${cep}':`, err)
+                this.cepErrorMessage = 'CEP inválido.'
+                this.loadingCep = false
+            }
+        })
     }
 }
