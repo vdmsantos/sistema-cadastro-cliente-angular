2c0bf0b8573cc16ef58a2a1381bbbee6e27bfb03
Luiz Comparin <luizcomparin18@gmail.com>
2024-01-13 17:17:47 -0300

    feat: add event listener input_on_keyup_enter

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ClienteFormService } from '../../services/cliente-form.service';
 
@@ -24,8 +24,14 @@ export class InputComponent {
     @Input() input_prefix: string = ''
     @Input() input_isDisabled: boolean = false
     @Input() input_maxLength: number = 60
-
+    @Output() input_on_keyup_enter: EventEmitter<string> = new EventEmitter();
     requiredErrorMessage = 'Este campo não pode estar vazio.'
+
+    emitEvent(event: Event | null) {
+        const inputElement = event?.target as HTMLInputElement
+        const inputValue = inputElement.value
+        this.input_on_keyup_enter.emit(inputValue)
+    }
 
     inputHasError() {
         return this.clienteService.checkIfFormControlHasError(this.input_formControl)
