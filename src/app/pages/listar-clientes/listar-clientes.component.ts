780327d86290b7b142c7ef8e98e627157fcdb722
Luiz Comparin <luizcomparin18@gmail.com>
2024-01-15 16:05:35 -0300

    change: abstract away listar-clientes page components

--- src/app/pages/listar-clientes/listar-clientes.component.ts
+++ src/app/pages/listar-clientes/listar-clientes.component.ts
@@ -3,10 +3,6 @@ import { ClienteEntity } from '../../entities/cliente.entity';
 import { ClienteService } from '../../services/cliente.service';
 import { PaginationOptionsType } from '../../types/paginationTypes/pagination-options.type';
 import { PaginationService } from '../../services/pagination.service';
-import { HelperService } from '../../services/helper.service';
-import { PrimeIcons } from 'primeng/api';
-import { MatDialog } from '@angular/material/dialog';
-import { DialogMobileListOptionsComponent } from '../../components/dialog-mobile-list-options/dialog-mobile-list-options.component';
 
 @Component({
     selector: 'app-listar-clientes',
@@ -17,8 +13,6 @@ export class ListarClientesComponent implements OnInit {
     constructor(
         private clienteService: ClienteService,
         private paginationService: PaginationService,
-        private helperService: HelperService,
-        private dialog: MatDialog
     ) { }
 
     ngOnInit(): void {
@@ -28,7 +22,6 @@ export class ListarClientesComponent implements OnInit {
     clientesListSignal = this.clienteService.getClientesListSignal
     paginationMetaSignal = this.paginationService.getPaginationMetaSignal
     lastPaginationOptionsUsedSig = this.paginationService.getLastPaginationOptionsUsedSig
-    PrimeIcons = PrimeIcons
 
     list_tableFields: { label: string, column: string }[] = [
         {
@@ -102,44 +95,6 @@ export class ListarClientesComponent implements OnInit {
                 estado: item.endereco.estado,
                 numero: item.endereco.numero,
             }
-        })
-    }
-
-    getRandomName() {
-        console.log('randomName', this.helperService.getRandomName())
-    }
-
-    add10Clientes() {
-        const getNewCliente = () => new ClienteEntity({
-            atualizado_em: new Date().toISOString(),
-            criado_em: new Date().toISOString(),
-            cpf: '99999999999',
-            id: this.helperService.newUUID(),
-            nome: this.helperService.getRandomName(),
-            telefone: '123456789',
-            endereco: {
-                bairro: 'aleatorio',
-                cep: '89012060',
-                cidade: 'aleatorio',
-                estado: 'aleatorio',
-                numero: '123',
-                rua: 'aleatorio'
-            },
-        })
-        const _10NewClientes = [getNewCliente(), getNewCliente(), getNewCliente(), getNewCliente(), getNewCliente(), getNewCliente(), getNewCliente(), getNewCliente(), getNewCliente(), getNewCliente(),]
-        _10NewClientes.forEach(cliente => {
-            this.clienteService.createOrUpdate(cliente)
-        })
-        this.fetchClientesList()
-    }
-
-    clearClienteList() {
-        this.clienteService.clearClienteList()
-    }
-
-    openSearchConfigDialog() {
-        this.dialog.open(DialogMobileListOptionsComponent, {
-            maxWidth: `100vw`,
         })
     }
 }
